f9aaf6120dcf14bd6d5a2798b4c171baee3383b9
POOJA <poojaaallam@gmail.com>
2024-01-05 16:36:22 +0530

    FIRST NEW COMMIT

---
+++ pooja/src/test/java/demo/First.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class First {
+
+	@Test
+	public void test() {
+		// TODO Auto-generated method stub
+		System.out.println("Pooja");
+
+	}
+
+}
